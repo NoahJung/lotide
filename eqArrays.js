21609c852e861f40d24dbefecd18013bbed113d5
Minjae Jung <noahjung87@gmail.com>
2021-03-03 05:54:20 +0000

    eqArray

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays( Arr1, Arr2 ) {
+
+  let result = false;
+  
+  if (Arr1.length === Arr2.length) {
+
+    for ( let i = 0; i < Arr1.length; i++){
+      if (Arr1[i] === Arr2[i]){
+        result = true;
+      } else {
+        result = false;
+        break;
+      }
+    }
+  
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+//TEST Code
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));  // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));  // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
