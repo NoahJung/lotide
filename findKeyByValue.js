065b2d57bbe1ed1afa030b57794b3aecb176d710
Minjae Jung <noahjung87@gmail.com>
2021-03-04 11:55:19 +0000

    done using for..in

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(obj, word) {
+  let result = "";
+
+  for (const item in obj) {
+    if(obj[item] === word) {
+      result = item;
+    } else {
+      result = undefined;
+    }
+  }
+  return result;
+}
+
+// TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+//console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
