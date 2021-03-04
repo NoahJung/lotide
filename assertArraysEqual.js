10a5269b7170ee034db2089705d62e236c7720bd
Minjae Jung <noahjung87@gmail.com>
2021-03-04 01:42:14 +0000

    edtied by mentor's assistance

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,41 @@
 
+function eqArrays( Arr1, Arr2 ) {
+
+  let result = false;
+  
+  if (Arr1.length === Arr2.length) {
+
+    for ( let i = 0; i < Arr1.length; i++){
+      if (Arr1[i] === Arr2[i]){
+        result = true;
+      } else {
+        result = false;
+        break;
+      }
+    }
+  
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+
+
 function assertArraysEqual( Arr1, Arr2 ) {
 
+  if (eqArrays(Arr1, Arr2)) {
+    return console.log(`${Arr1} ✅=== ${Arr2}, so it is true.`);
+  } else {
+    return console.log(`${Arr1} 🛑!== ${Arr2}, so it is false.`);
+  }
+}
+
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,2,"3"]);
+
+  
+/*
   let result = false;
   
   if (Arr1.length === Arr2.length) {
@@ -21,7 +56,6 @@ function assertArraysEqual( Arr1, Arr2 ) {
     console.log(`The length of arrays are not equal. So it is false.`)
   }
   return result;
-}
+  */
 
-//assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,2,"3"]);
+
--- without.js
+++ without.js
@@ -1,4 +1,39 @@
 
+function eqArrays( Arr1, Arr2 ) {
+
+  let result = false;
+  
+  if (Arr1.length === Arr2.length) {
+
+    for ( let i = 0; i < Arr1.length; i++){
+      if (Arr1[i] === Arr2[i]){
+        result = true;
+      } else {
+        result = false;
+        break;
+      }
+    }
+  
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+
+
+function assertArraysEqual( Arr1, Arr2 ) {
+
+  if (eqArrays(Arr1, Arr2)) {
+    return console.log(`${Arr1} ✅=== ${Arr2}, so it is true.`);
+  } else {
+    return console.log(`${Arr1} 🛑!== ${Arr2}, so it is false.`);
+  }
+}
+
+
+
+
 function without( Arr1, Arr2 ) {
 
   let removeArr = Arr2;
@@ -20,13 +55,24 @@ function without( Arr1, Arr2 ) {
   for ( let i = 0; i < Arr1.length; i++){
     if (Arr1[i] === removeArr[i]){
       result.splice(i,1);
-      console.log(result);
+      //console.log(result);
     } 
   }
+
+  return result;
 }
 
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
+//console.log(without([1, 2, 3], [1]));
 // FUNCTION IMPLEMENTATION
 
 
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+//without([1, 2, 3], [1]) // => [2, 3]
+//without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
