35fe520fef698601184ed7af4b21ecef68a42ff2
Minjae Jung <noahjung87@gmail.com>
2021-03-09 14:18:51 +0000

    refactor eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+//TEST Code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]));  // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));  // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
