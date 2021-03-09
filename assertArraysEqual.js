88b6588d38d4885562f975734d5db870deddb53a
Minjae Jung <noahjung87@gmail.com>
2021-03-09 14:24:12 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,4 @@
-
-function eqArrays( Arr1, Arr2 ) {
-
-  let result = false;
-  
-  if (Arr1.length === Arr2.length) {
-
-    for ( let i = 0; i < Arr1.length; i++){
-      if (Arr1[i] === Arr2[i]){
-        result = true;
-      } else {
-        result = false;
-        break;
-      }
-    }
-  
-  } else {
-    result = false;
-  }
-  return result;
-}
-
+const eqArrays = require('./eqArrays');
 
 
 function assertArraysEqual( Arr1, Arr2 ) {
@@ -31,31 +10,9 @@ function assertArraysEqual( Arr1, Arr2 ) {
   }
 }
 
+module.exports = assertArraysEqual;
+
 assertArraysEqual([1,2,3],[1,2,3]);
 assertArraysEqual([1,2,3],[1,2,"3"]);
-
-  
-/*
-  let result = false;
-  
-  if (Arr1.length === Arr2.length) {
-
-    for ( let i = 0; i < Arr1.length; i++){
-      if (Arr1[i] === Arr2[i]){
-        result = true;
-        console.log(`index ${i} : ${Arr1[i]} ✅=== ${Arr2[i]}, so it is true.`);
-      } else {
-        result = false;
-        console.log(`index ${i} : ${Arr1[i]} 🛑!== ${Arr2[i]}, so it is false.`);
-        break;
-      }
-    }
-  
-  } else {
-    result = false;
-    console.log(`The length of arrays are not equal. So it is false.`)
-  }
-  return result;
-  */
 
 
